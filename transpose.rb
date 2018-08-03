8a6ee1842bea897559ae4e9680fd2bb006ef0810
Sander Ploegsma <sanderploegsma@gmail.com>
2018-08-03 12:24:39 +0200

    Update transpose.rb

--- transpose.rb
+++ transpose.rb
@@ -3,7 +3,7 @@ class Transpose < Formula
   homepage "https://github.com/sanderploegsma/transpose"
   url "https://github.com/sanderploegsma/transpose/releases/download/v0.1/transpose_v0.1_darwin_amd64.tar.gz"
   version "0.1"
-  sha256 "077b527a4334d58cb19a0fca109aba1f2027ca41205f356bfd831c0c2c65e261"
+  sha256 "90168b49dac1004a4beee09e142a6344f92391750f2c431fe59fb54e34303582"
 
   def install
     bin.install "transpose"
