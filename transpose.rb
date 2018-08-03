bf4214a5c0a098a641ac88edbe3100ea772c5077
Sander Ploegsma <sanderploegsma@gmail.com>
2018-08-03 16:13:12 +0200

    Update transpose to v0.2

--- transpose.rb
+++ transpose.rb
@@ -1,9 +1,9 @@
 class Transpose < Formula
   desc "Transposes tables or matrices from the command line"
   homepage "https://github.com/sanderploegsma/transpose"
-  url "https://github.com/sanderploegsma/transpose/releases/download/v0.1/transpose_v0.1_darwin_amd64.tar.gz"
-  version "0.1"
-  sha256 "90168b49dac1004a4beee09e142a6344f92391750f2c431fe59fb54e34303582"
+  url "https://github.com/sanderploegsma/transpose/releases/download/v0.2/transpose_v0.1_darwin_amd64.tar.gz"
+  version "0.2"
+  sha256 "5f112fa9925e5b593b3a9ef7b60fa395c6c5276cf58faab5b6ea9552dc48226c"
 
   def install
     bin.install "transpose"
