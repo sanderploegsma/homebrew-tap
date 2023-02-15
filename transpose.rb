e1886aa5253169ff355351926962ce61f6f87369
Sander Ploegsma <sanderploegsma@gmail.com>
2023-02-15 14:29:19 +0100

    Update Transpose to v0.3 with M1 support

--- transpose.rb
+++ transpose.rb
@@ -1,9 +1,19 @@
 class Transpose < Formula
   desc "Transposes tables or matrices from the command line"
   homepage "https://github.com/sanderploegsma/transpose"
-  url "https://github.com/sanderploegsma/transpose/releases/download/v0.2/transpose_v0.2_darwin_amd64.tar.gz"
-  version "0.2"
-  sha256 "5f112fa9925e5b593b3a9ef7b60fa395c6c5276cf58faab5b6ea9552dc48226c"
+  version "0.3"
+
+  on_macos do
+    on_intel do
+      url "https://github.com/sanderploegsma/transpose/releases/download/v0.3/transpose-v0.3-darwin-amd64.tar.gz"
+      sha256 "225e9878d4a683d6dc58d407ca8ccbb0897e48a18d893a88c84fc8516f8d5def"
+    end
+
+    on_arm do
+      url "https://github.com/sanderploegsma/transpose/releases/download/v0.3/transpose-v0.3-darwin-arm64.tar.gz"
+      sha256 "7d9cd2e029bc481fda7fbbb894b1e8c6c8c7ed3efcf69c37dc801937d317e6fa"
+    end
+  end
 
   def install
     bin.install "transpose"
